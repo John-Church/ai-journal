2433ab6a12ebd6e0363f9641ac4a04823e7240dd
John-Church <churchjohnr@gmail.com>
2023-03-05 18:05:43 -0800

    frontend init

---
+++ frontend/web/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full h-screen font-serif">
+    <div class="mx-auto flex justify-between max-w-7xl px-8 py-4 items-center">
+      <h1 class="font-sans text-2xl font-bold">Mnemo</h1>
+      <div class="space-x-8">
+        <NuxtLink to="/">Record Journal</NuxtLink>
+        <NuxtLink to="/entries">Entries</NuxtLink>
+      </div>
+    </div>
+    <slot />
+  </div>
+</template>
---
+++ frontend/web/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="max-w-7xl px-8 mx-auto">Journal Recording</div>
+</template>
